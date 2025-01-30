1f12dd5fa1a434baa5a4bc22b36bb54debfc4637
Yanis ADEKALOM <yanis971.60@gmail.com>
2025-01-30 09:08:06 +0100

    Feat: Clean

--- src/main/java/com/example/Residences/repositories/PersonneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Residences.repositories;
-
-import com.example.Residences.entities.identification.Personne;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface PersonneRepository extends JpaRepository<Personne, Long> {
-}
